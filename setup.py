52664bef7c717c06e91cdf232df22aa70e1c5fe0
Daniel Reis <dreis.pt@hotmail.com>
2017-04-30 17:17:12 +0100

    Top setup.py packaging full repo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="odoo10-oxide",
     description="Meta package for Odoo 10 Oxide addons",
-    version="10.0",
+    version="10.0.0.1.0",
     setup_requires=['setuptools-odoo'],
     odoo_addons=True,
 )
